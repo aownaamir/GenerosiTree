6a4bde18de473589e78276134c14271734e6e77e
Aown-Aamir <aownaamirkips@gmail.com>
2024-09-27 06:54:16 +0500

    second draft

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Departments from "./ui/Departments";
 import AboutUs from "./ui/AboutUs";
 import Links from "./ui/Links";
 import Footer from "./ui/Footer";
+import JoinTeam from "./ui/JoinTeam";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <Departments />
       <AboutUs />
+      <JoinTeam />
       <Links />
       <Footer />
     </div>
